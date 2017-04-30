74878bcb27d7eb29d75aada0257d1d1afb8cf5d3
zwenderk <zwenderk@hotmail.com>
2017-05-01 00:21:54 +0200

    Un cuadrilátero

--- main/main.go
+++ main/main.go
@@ -2,17 +2,14 @@ package main
 
 import (
     "fmt"
-
     "log"
-
     "runtime"
-
-    "github.com/go-gl/gl/v2.1/gl" // OpenGl antiguo
+    "github.com/go-gl/gl/v2.1/gl" // OpenGL antiguo
     "github.com/go-gl/glfw/v3.2/glfw"
 )
 
 const (
-    tituloVentana = "01_Golang, activando el espacio de trabajo"
+    tituloVentana = "02_Golang, un cuadrilátero"
     anchoVentana = 640
     altoVentana = 480
 )
@@ -20,14 +17,17 @@ const (
 //*********************************************************************************************************
 // Función que se llama después de haber evaluado todas las declaraciones de variables del paquete y antes que main
 
-func init() {
-    // manejo de eventos GLFW se debe ejecutar en el hilo principal
-    runtime.LockOSThread()
-}
+//func init() {
+// manejo de eventos GLFW se debe ejecutar en el hilo principal
+//    runtime.LockOSThread()
+//}
 
 //*********************************************************************************************************
 
 func main() {
+
+    runtime.LockOSThread()
+
     if err := glfw.Init(); err != nil {
         // Inicializa GLFW
         log.Fatalln("Fallo al inicializar glfw:", err) // Si hay error informar
@@ -36,10 +36,10 @@ func main() {
 
     // Valores que tendrá la ventana WindowHint(variable, valor que tendrá esa variable)
     glfw.WindowHint(glfw.Resizable, glfw.False)                 // No cambia de tamaño
-    glfw.WindowHint(glfw.ContextVersionMajor, 4)                // Versión mayor permitida
+    glfw.WindowHint(glfw.ContextVersionMajor, 2)                // Versión mayor permitida
     glfw.WindowHint(glfw.ContextVersionMinor, 1)                // Versión menor permitida
-    glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile) // Usar el profile OpenGL Core
-    glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+    //glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile) // Usar el profile OpenGL Core
+    //glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
 
     window, err := glfw.CreateWindow(// Crear ventana
         anchoVentana,
@@ -60,18 +60,35 @@ func main() {
     version := gl.GoStr(gl.GetString(gl.VERSION))
     fmt.Println("OpenGL versión", version) // Imprime versión de OpenGL del sistema
 
+    gl.ClearColor(.5, 1, 0, 0.0) // Especifica valores de color de limpieza
+
     // -------------> BUCLE PRINCIPAL
     for !window.ShouldClose() {
 
         dibuja()
 
         // Mantenimiento
-        window.SwapBuffers() // Intercambia buffers para presentar en pantalla
-        glfw.PollEvents() // Procesa aquellos eventos que ya se han recibido y luego retorna inmediatamente
-    }
+        window.SwapBuffers() // Intercambia buffers para presenter en pantalla
+        glfw.PollEvents()
+    } // -----------> FIN DE BUCLE PRINCIPAL
 }
 
-func dibuja() { // Mas adelante la usaremos para dibujar
-    gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-}
+func dibuja() {
+    gl.Clear(gl.COLOR_BUFFER_BIT)
 
+    gl.Begin(gl.QUADS) // Cada coordenada con su color
+
+    gl.Color4f(1, 0, 0, 0)
+    gl.Vertex2f(-0.5, 0.5)
+
+    gl.Color4f(0, 1, 0, 0)
+    gl.Vertex2f(0.5, 0.5)
+
+    gl.Color4f(0, 0, 1, 0)
+    gl.Vertex2f(0.5, -0.5)
+
+    gl.Color4f(1, 1, 1, 0)
+    gl.Vertex2f(-0.5, -0.5)
+
+    gl.End()
+}
